f17453fffa91253458f94f42d259f9ce12591f08
vishakha011 <khatadevishakha01@gmail.com>
2020-11-10 13:04:56 +0530

    add setHeader

--- block-BNaaaz/file.js
+++ block-BNaaaz/file.js
@@ -4,8 +4,9 @@ var fs = require('fs');
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
+    res.setHeader('Content-Type', 'text/plain')
     fs.createReadStream('./readme.txt').pipe(res);
-    
+
 }
 
 server.listen(3000, 'localhost', () => {
